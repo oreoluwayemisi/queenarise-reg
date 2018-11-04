5643407829f16d2b8927dd908f41d45be98c17d9
Benson Imoh, ST <benson@stbensonimoh.com>
2018-11-04 03:22:45 +0100

    Capitalize the First Letter of Input fields in both CSS and JavaScript
    * add an on-change event to capitaize the first letter or input fields.
    * add a css style to do the same thing for the look.
    * put form data into JavaScript variables.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function () {
     // DOM is fully loaded
+
+    // Capitalize the first letter of First Name
+    $('#firstName').on('change', function(e) {
+        var $this = $(this),
+            val = $this.val();
+            regex = /\b[a-z]/g;
+
+        val = val.charAt(0).toUpperCase() + val.substr(1);
+    });
+
+    // Capitalize the first letter of Last Name
+    $('#lastName').on('change', function (e) {
+        var $this = $(this),
+            val = $this.val();
+        regex = /\b[a-z]/g;
+
+        val = val.charAt(0).toUpperCase() + val.substr(1);
+    });
+    
+
+    // Capitalize the first letter of City
+    $('#city').on('change', function (e) {
+        var $this = $(this),
+            val = $this.val();
+        regex = /\b[a-z]/g;
+
+        val = val.charAt(0).toUpperCase() + val.substr(1);
+    });
+
+    // change the email to lowercase
+    $('#email').on('change', function(e) {
+        var $this = $(this),
+            val = $this.val();
+            val = val.toLowerCase();
+    });
+
     // On clicking the empress option, select the option, toggle the display of the list and hide other packages
     $('#empress').click(function(e){
         $div = $('#empressList');
@@ -20,5 +56,18 @@ $(document).ready(function () {
         $($div).toggle();
         $('#queen').prop('checked',true);
         $('ul').not($div).hide();
+    });
+
+    //When form is submitted, grab data
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        // put form data into variables
+        var firstName = $.trim(document.getElementById('firstName').value);
+        var lastName = $.trim(document.getElementById('lastName').value);
+        var email = $.trim(document.getElementById('email').value);
+        var phone = $.trim(document.getElementById('phone').value);
+        var city = $.trim(document.getElementById('city').value);
+        var ticketType = document.querySelector('input[name="ticketType"]:checked').value;
     });
 });
